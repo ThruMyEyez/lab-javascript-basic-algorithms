bd1883cc1c7d25ed2c7570fb5aa9a70e920794a2
Artur Baumeister <Artur.baumeister@gmail.com>
2022-08-04 21:00:02 +0200

    Update 3. loops solved

--- js/index.js
+++ js/index.js
@@ -22,10 +22,10 @@ if (hacker1.length == hacker2.length) {
 
 let separated = "";
 for (let i = 0; i < hacker1.length; i++) {
-  console.log(hacker1.charAt(i));
+  //console.log(hacker1.charAt(i));
   separated = separated + hacker1[i].toUpperCase();
   if (i != hacker1.length - 1) {
-    console.log(i);
+    //console.log(i);
     separated = separated + " ";
   }
 }
@@ -33,3 +33,27 @@ for (let i = 0; i < hacker1.length; i++) {
 console.log(
   `Driver name length:${hacker1.length} - Result length: ${separated.length} - Result: ${separated}`
 );
+
+// solution for 3.2
+let reversed = "";
+for (let i = hacker2.length - 1; i >= 0; i--) {
+  reversed += hacker2[i];
+}
+console.log(reversed);
+
+// solution for 3.3
+const driver = "The driver's name goes first.",
+  navigator = "Yo, the navigator goes first definitely.",
+  sameName = "What?! You both have the same name?";
+
+let x = hacker1.localeCompare(hacker2);
+console.log(x);
+console.log("Felix".localeCompare("Fargo"));
+if (x === 0) {
+  sameName;
+  console.log(sameName);
+} else if (x === 1) {
+  console.log(navigator);
+} else {
+  console.log(driver);
+}
